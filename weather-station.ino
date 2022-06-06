390401e60d3c50260cc9454167a74f13fa9fa4af
Davi Schmitz <davi.schmitz@hotmail.com>
2022-06-06 20:19:07 -0300

    Use BMP180 to get pressure readings

--- weather-station.ino
+++ weather-station.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>                                                  
 #include <DHT.h>
-#include "Seeed_BMP280.h"
+#include <Adafruit_BMP085.h>
 #include <WiFiMulti.h>
 WiFiMulti wifiMulti;
 #define DEVICE "ESP32"
@@ -8,48 +8,49 @@ WiFiMulti wifiMulti;
 #include <InfluxDbClient.h>
 #include <InfluxDbCloud.h>
 
-#define WIFI_SSID "<Network Name>"                                                                                        //Network Name
-#define WIFI_PASSWORD "<Network Password>"                                                                                //Network Password
-#define INFLUXDB_URL "<INFLUXDB_URL>"                                                                                     //InfluxDB v2 server url, e.g. https://eu-central-1-1.aws.cloud2.influxdata.com (Use: InfluxDB UI -> Load Data -> Client Libraries)
-#define INFLUXDB_TOKEN "<INFLUXDB_TOKEN>"                                                                                 //InfluxDB v2 server or cloud API token (Use: InfluxDB UI -> Data -> API Tokens -> <select token>)
-#define INFLUXDB_ORG "<INFLUXDB_ORG>"                                                                                     //InfluxDB v2 organization id (Use: InfluxDB UI -> User -> About -> Common Ids )
-#define INFLUXDB_BUCKET "weather-station"                                                                               //InfluxDB v2 bucket name (Use: InfluxDB UI ->  Data -> Buckets)
-#define TZ_INFO "BRST+3BRDT+2,M10.3.0,M2.3.0"                                                                                                 //InfluxDB v2 timezone
+#define WIFI_SSID "<Network Name>"                                                                                        
+#define WIFI_PASSWORD "<Network Password>"                                                                               
+#define INFLUXDB_URL "<INFLUXDB_URL>"                                                                                     
+#define INFLUXDB_TOKEN "<INFLUXDB_TOKEN>"           
+#define INFLUXDB_ORG "davi.schmitz2@gmail.com"                                                                   
+#define INFLUXDB_BUCKET "weather-station"                                                                        
+#define TZ_INFO "BRST+3BRDT+2,M10.3.0,M2.3.0"                                                                    
 
-DHT dht(15,DHT11);                                                   //DHT and BMP sensor parameters
-BMP280 bmp280;
+DHT dht(15,DHT11);                                                   
+Adafruit_BMP085 bmp180;
 
-int temp = 0;                                                       //Variables to store sensor readings
+int temp = 0;                                                       
 int humid = 0;
 int pressure = 0;
 
-InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);                 //InfluxDB client instance with preconfigured InfluxCloud certificate
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);                 
 
-Point sensor("weather");                                            //Data point
+Point sensor("weather");                                            
 
 void setup() {
-  Serial.begin(115200);                                             //Start serial communication
+  Serial.begin(115200);                                             
   
-  dht.begin();                                                      //Connect to the DHT Sensor
-//  if(!bmp280.init())                                                //Connect to pressure sensor
-//    Serial.println("bmp280 init error!");
+  dht.begin();                                                      
+  if(!bmp180.begin()) {                                             
+    Serial.println("bmp280 init error!");
+  }
 
-  WiFi.mode(WIFI_STA);                                              //Setup wifi connection
+  WiFi.mode(WIFI_STA);                                              
   wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.print("Connecting to wifi");                               //Connect to WiFi
+  Serial.print("Connecting to wifi");                               
   while (wifiMulti.run() != WL_CONNECTED) {
     Serial.print(".");
     delay(100);
   }
   Serial.println();
 
-  sensor.addTag("device", DEVICE);                                   //Add tag(s) - repeat as required
+  sensor.addTag("device", DEVICE);                                   
   sensor.addTag("SSID", WIFI_SSID);
 
-  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");                 //Accurate time is necessary for certificate validation and writing in batches
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");                 
 
-  if (client.validateConnection()) {                                 //Check server connection
+  if (client.validateConnection()) {                                 
     Serial.print("Connected to InfluxDB: ");
     Serial.println(client.getServerUrl());
   } 
@@ -59,31 +60,31 @@ void setup() {
   }
 }
 
-void loop() {                                                         //Loop function 
-  temp = dht.readTemperature();                                      //Record temperature
-  humid = dht.readHumidity();                                        //Record temperature
-//  pressure = bmp280.getPressure()/100;                               //Record pressure
+void loop() {                                                        
+  temp = dht.readTemperature();                                      
+  humid = dht.readHumidity();                                        
+  pressure = bmp180.readPressure()/100;                              
 
-  sensor.clearFields();                                              //Clear fields for reusing the point. Tags will remain untouched
+  sensor.clearFields();                                              
 
-  sensor.addField("temperature", temp);                              // Store measured value into point
-  sensor.addField("humidity", humid);                                // Store measured value into point
-//  sensor.addField("pressure", pressure);                             // Store measured value into point
+  sensor.addField("temperature", temp);                              
+  sensor.addField("humidity", humid);                                
+  sensor.addField("pressure", pressure);                             
 
     
-  if (wifiMulti.run() != WL_CONNECTED)                               //Check WiFi connection and reconnect if needed
+  if (wifiMulti.run() != WL_CONNECTED)                               
     Serial.println("Wifi connection lost");
 
-  if (!client.writePoint(sensor)) {                                   //Write data point
+  if (!client.writePoint(sensor)) {                                  
     Serial.print("InfluxDB write failed: ");
     Serial.println(client.getLastErrorMessage());
   }
   
-  Serial.print("Temp: ");                                            //Display readings on serial monitor
+  Serial.print("Temp: ");                                            
   Serial.println(temp);
   Serial.print("Humidity: ");
   Serial.println(humid);
-//  Serial.print("Pressure: ");
-//  Serial.println(pressure);
-  delay(10000);                                                      //Wait 60 seconds
+  Serial.print("Pressure: ");
+  Serial.println(pressure);
+  delay(1000);                                                      
 }
